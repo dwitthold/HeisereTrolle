09d6d5215c4846d82c9b7f368ba0da56b39f106b
dwitthold <witthold@menkar.uberspace.de>
2022-03-22 10:15:36 +0100

    Initial version

---
+++ content_scripts/hide_trolls.js
@@ -0,0 +1,7 @@
+(function() {
+    let elementList = document.getElementsByName('meldung.newsticker.bottom.kommentarelesen');
+    let comment = elementList[0];
+    
+    comment.style.display = 'none';
+}
+)();
